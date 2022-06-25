b3893104faabf4688a160e95c58d1bfcf1c28835
NickeaBennett <nickeabennett@gmail.com>
2022-06-25 19:04:26 -0400

    Clean layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
       <h1>Hello World!</h1>
+
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   );
 }
