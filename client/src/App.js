a67ca55ce2c879d3dd849182d7dbeacf4378d706
NickeaBennett <nickeabennett@gmail.com>
2022-06-25 20:00:19 -0400

    Installed Material UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,32 @@ import * as React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
+import PersistentDrawerLeft from "./components/drawer/Drawer ";
 import Navbar from './components/navbar/Navbar';
+import Homepage from "./components/homepage/Homepage";
+import Cryptocurrency from "./components/cryptocurency/Cryptocurrency";
+import Favorites from "./components/favorites/Favorites";
 
 function App() {
   return (
     <div className="App">
-      <div className="navbar">
+      <div className="Navbar">
         <Navbar />
+        <PersistentDrawerLeft />
       </div>
-      <div className="main">
-      <h1>Hello World!</h1>
+      <div className="Main">
+          <h1>Hello World!</h1>
 
-      </div>
-      <div className="footer">
+        <div className="Routes">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/cryptocurency" element={<Cryptocurrency />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </div>
+        <div className="Footer">
 
+        </div>
       </div>
     </div>
   );
---
+++ client/src/components/cryptocurency/Cryptocurrency.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cryptocurrency = () => {
+  return (
+    <div>
+        <h1>Cryptocurrency</h1> 
+    </div>
+  )
+}
+
+export default Cryptocurrency;
